402ca506726ae12e2c9552fb33a30c64cd9a11bb
rgliuca <44307228+rgliuca@users.noreply.github.com>
2021-07-07 11:53:43 -0700

    Update pygame_starter.py

--- pygame_starter.py
+++ pygame_starter.py
@@ -3,6 +3,8 @@ import pygame
 screen = pygame.display.set_mode((800,800))
 pygame.display.set_caption("Draw Basic Objects")
 GREEN = (0, 255, 0) # (R, G, B)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 done = False
 while not done:
@@ -10,10 +12,18 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
             break
-    
+
+    '''
+    key_pressed = pygame.key.get_pressed()
+    if key_pressed[pygame.K_q]:
+        done = True
+    '''
     #screen.fill((255, 255, 255))
     pygame.draw.line(screen, GREEN, (200, 200), (400, 400))
+    pygame.draw.circle(screen, RED, (400, 200), 50, 0)
+    pygame.draw.rect(screen, BLUE, pygame.Rect(300, 200, 350, 250), 5)
 
     pygame.display.flip()
 
 pygame.quit()
+
