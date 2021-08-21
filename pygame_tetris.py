86b4f6ad59c146af46161442c8c6ecb2d742e9dd
rgliuca <44307228+rgliuca@users.noreply.github.com>
2021-08-21 11:02:22 -0700

    Update pygame_tetris.py

--- pygame_tetris.py
+++ pygame_tetris.py
@@ -1,13 +1,15 @@
 from pygame_objects import *
+import pygame
+import copy
+import time
 
 class TetrisTile(Tile):
-	TETRIS_TILE_SIZE = 50 
+	TILE_SIZE = 50 
 
 	def __init__(self, position, display, color):
-		super().__init__(position, display)
-		self._tile_size = __class__.TETRIS_TILE_SIZE
-		self.color = color
-		self.rasterize()
+		super().__init__(
+			position, display, 
+			__class__.TETRIS_TILE_SIZE, color)
 
 class TetrisBlock:
 	'''
@@ -15,43 +17,137 @@ class TetrisBlock:
 	consisting of 16 tiles
 	'''
 
-	BLOCKS = [
-		# block 0
-		(	
-		[[0, 1, 0, 0],
-		 [0, 1, 0, 0],
-		 [0, 1, 0, 0], 
-		 [0, 1, 0, 0]],
-	 	(0, 0, 255))
+	TILE_GAP = 5
 
-		# block 1
-		
+	BLOCKS = [
+        # block 0
+        (
+            [[0, 1, 0, 0],
+             [0, 1, 0, 0],
+             [0, 1, 0, 0],
+             [0, 1, 0, 0]],
+            (2, 117, 187)),
+
+        # block 1
+        (
+            [[0, 0, 0, 0],
+             [0, 1, 1, 0],
+             [0, 1, 1, 0],
+             [0, 0, 0, 0]],
+            (53, 187, 238)),
+
+        # block 2
+        (
+            [[1, 1, 0, 0],
+             [0, 1, 0, 0],
+             [0, 1, 0, 0],
+             [0, 0, 0, 0]],
+            (3, 152, 137)),
+
+        # block 3
+        (
+            [[0, 1, 1, 0],
+             [0, 1, 0, 0],
+             [0, 1, 0, 0],
+             [0, 0, 0, 0]],
+            (240, 120, 52)),
+
+        # block 4
+        (
+            [[0, 1, 0, 0],
+             [1, 1, 1, 0],
+             [0, 0, 0, 0],
+             [0, 0, 0, 0]],
+            (206, 50, 17)),
+
+        # block 5
+        (
+            [[0, 1, 0, 0],
+             [1, 1, 0, 0],
+             [1, 0, 0, 0],
+             [0, 0, 0, 0]],
+            (239, 51, 120)),
+
+        # block 6
+        (
+            [[1, 0, 0, 0],
+             [1, 1, 0, 0],
+             [0, 1, 0, 0],
+             [0, 0, 0, 0]],
+            (138, 32, 85))
 	]
 
-	def __init__(self, position, display):
-		#self._block_id = ....
-		# self._block_id should be a random value between 1 and 7
-		# if block_id is 1, then the matrix should be
+	def __init__(self, position, display, block_id=None):
+		# if block_id is None, select a random block from
+		# the list __class__.BLOCKS
+		# otherwise, assign self._block_id = block id  
+		# This is useful for debugging! 
+		# self._block_id = block_id
 
-		# self._block should be a 4x4 matrix of 0s and 1s
-		if self._block_id == 1:
-			self._block = [[0, 1, 0, 0] for _ in range(4)]
-		#elif....
+		self._x, self._y = position
+		self._display = display
+
+		if block_id is None or \
+			block_id < 0 or \
+			block_id >= len(self.BLOCKS):
+			raise ValueError
+
+		self._block = copy.deepcopy(self.BLOCKS[block_id])
+
 
 	def rotate_left(self):
+		# rotates the block by 90 degrees left
+		# and calls rasterize to display on the canvas
 		pass
 
 	def rotate_right(self):
+		# rotates the block by 90 degrees right
+		# and calls rasterize to display on the canvas
 		pass
 
 	def move_block_down(self):
+		# moves the block down by one unit in the y direction (animate)
+		# and calls rasterize to display the block on the canvas
+		pass
+
+	def rasterize(self):
+		# This method is going to take the display
+		# and display this Tetris block at self._x, self._y
+
+		# take self._block (the 2D list) and generate the TetrisBlock 
+		# at self._x, self._y position
+
+		# loop through self._block (image)
 		pass
 
 
+class TetrisGame:
+	BKGROUND_COLOR = (255, 255, 255)
+
+	def __init__(self, display):
+		self._last_block_id = 0
+		self._display = display
+		self._dsplay.fill(TetrisGame.BKGROUND_COLOR)
+		pygame.display.set_caption("TETRIS")
+
 if __name__ == "__main__":
-	disp = pygame.display.set_mode((400, 300), 0, 32)
-	#tile = Tile((10, 10), disp)
-	tile = TetrisTile((255, 0, 0), (10, 10), disp)
+	canvas_height = 800
+	canvas_width = 400
+	disp = pygame.display.set_mode((canvas_width, canvas_height), 0, 32)
+	
+	disp.fill(TetrisGame.BKGROUND_COLOR)
+	block = TetrisBlock((100, 0), disp, 0)
+
+	block.rasterize()
+
+	loop_delay = 0.05
+	count = 0
+
+	t1 = TetrisTile((10, 10), disp, (255,0,0))
+
+	time.sleep(5)
+	pygame.quit()
+	quit()
 
 	while True:
 		for event in pygame.event.get():
@@ -60,5 +156,15 @@ if __name__ == "__main__":
 				break
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				print(pygame.mouse.get_pos())
-		
+
+			if event.type == pygame.K_LEFT:
+				block.rotate_left()
+
+		if count * loop_delay > 1:
+			count = 0
+			# clear the screen
+			# call TetrisBlock's method to move the block down by one unit!
+
+		count += 1
+		time.sleep(loop_delay)
 		
